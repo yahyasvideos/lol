2223fa66015a7d62bcda21433e6f430a7c2c7653
yahyasvideos <99501233+yahyasvideos@users.noreply.github.com>
2022-02-11 20:22:15 +0200

    Add files via upload

---
+++ the question.bat
@@ -0,0 +1,31 @@
+@echo off
+title the HARDEST QUESTION EVER
+echo the
+echo question
+echo is
+
+set /p menu="Are you stupid? [yes/no]"
+
+       if %menu%==yes goto UserAcionIAI
+       if %menu%==no goto UserActions2
+       cls
+
+:UserAcionIAI
+so you stupid....good
+
+pause
+
+echo you got the (wasting time ending) enter the code 8374 in codez.bat
+
+:UserActions2
+echo you won thanks for letting me waste your time
+
+pause
+
+echo this is the frist game I make
+
+echo if you wanna me to make gamez like this dont please subscribe
+
+echo and leave a like
+
+pause
